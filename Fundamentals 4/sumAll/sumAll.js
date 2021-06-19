4d8fa30ddd2a63c16f9fa9ded950283c5c26277c
Vaia-Pasch <lemonpas@outlook.com>
2021-06-20 00:14:21 +0300

    incomplete exercise of Fundamentals 4 (local)

--- Fundamentals 4/sumAll/sumAll.js
+++ Fundamentals 4/sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
-}
+const sumAll = function(smallest, largest) {
+        let total = smallest + largest;
+        if (total < 0); {
+            return "ERROR"
+        } 
+        let n = (largest - smallest + 1);
+        let sum = n * (smallest + largest) / 2; 
+    
+        return sum;
+    }
 
 module.exports = sumAll
--- Fundamentals 4/tempConversion/tempConversion.js
+++ Fundamentals 4/tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const ftoc = function() {
+const ftoc = function(fahrenheit) {
+  let farTemp = (5/9) *(fahrenheit - 32);
+  let resultFar = Math.round( farTemp * 10) / 10;
+  return resultFar;
 
 }
 
-const ctof = function() {
+const ctof = function(celcius) {
+  let celTemp = celcius * 9/5 + 32;
+  let resultCel = Math.round( celTemp * 10) / 10;
+  return resultCel;
 
 }
 
